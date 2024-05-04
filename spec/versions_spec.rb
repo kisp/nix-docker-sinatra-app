6cdf50af6d8b5d93bf332c42a3646d0f7972e508
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-04 09:10:58 +0200

    spec/versions_spec.rb

--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -2,14 +2,18 @@ require "sinatra"
 
 RSpec.describe "versions" do
   describe "ruby" do
-    it "has version 3.1.4" do
-      expect(RUBY_VERSION).to eq("3.1.4")
+    let(:expected) { "3.1.4" }
+
+    it "has version #{expected}" do
+      expect(RUBY_VERSION).to eq(expected)
     end
   end
 
   describe "sinatra" do
-    it "has version 4.0.0" do
-      expect(Sinatra::VERSION).to eq("4.0.0")
+    let(:expected) { "4.0.0" }
+
+    it "has version #{expected}" do
+      expect(Sinatra::VERSION).to eq(expected)
     end
   end
 end
